6f5bd4e5bcbb80162b01e291e5e028c5979d024b
MaxAwan <MaxAwan@users.noreply.github.com>
2022-10-24 12:04:25 +0100

    Update _setup.py
    pin incremental for use in crawler

--- src/twisted/python/_setup.py
+++ src/twisted/python/_setup.py
@@ -239,7 +239,7 @@ def getSetupArgs(extensions=_EXTENSIONS):
     requirements = [
         "zope.interface >= 4.4.2",
         "constantly >= 15.1",
-        "incremental >= 16.10.1",
+        "incremental>=16.10.1,<=21.3.0",
         "Automat >= 0.3.0",
         "hyperlink >= 17.1.1",
         "PyHamcrest >= 1.9.0",
@@ -249,7 +249,7 @@ def getSetupArgs(extensions=_EXTENSIONS):
     arguments.update(dict(
         packages=find_packages("src"),
         use_incremental=True,
-        setup_requires=["incremental >= 16.10.1"],
+        setup_requires=["incremental>=16.10.1,<=21.3.0"],
         install_requires=requirements,
         entry_points={
             'console_scripts': _CONSOLE_SCRIPTS
